803b1aac8a3e746d6f103958be9dc1b1c64d8a08
andTheowind <59265638+andTheowind@users.noreply.github.com>
2021-09-15 19:12:39 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@ const wrapper = document.querySelector('.wrapper');
 const firstMenuItem = document.querySelector('nav > ul li');
 const secondMenuItem = document.querySelector('nav > ul li + li');
 const thirdMenuItem = document.querySelector('nav > ul li + li +li');
+const nav = document.querySelector('nav');
 
 openButton.addEventListener('click', function () {
     wrapper.classList.add('show-nav');
     firstMenuItem.style.transform = 'translateX(0%)';
     secondMenuItem.style.transform = 'translateX(0%)';
     thirdMenuItem.style.transform = 'translateX(0%)';
-
+    nav.style.display = 'none';
 })
 
 closeButton.addEventListener('click', function () {
@@ -18,4 +19,5 @@ closeButton.addEventListener('click', function () {
     firstMenuItem.style.transform = 'translateX(-100%)';
     secondMenuItem.style.transform = 'translateX(-150%)';
     thirdMenuItem.style.transform = 'translateX(-200%)';
+    nav.style.display = 'block';
 })
